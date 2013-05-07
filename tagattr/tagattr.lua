623f1f4068dd60d38e0125795fa8ea3093de605d
rumia <rumia.youkai.of.dusk@gmail.com>
2013-05-07 14:09:31 +0700

    tagattr: add prefix/suffix to messages
    - you can use `plugins.var.lua.tagattr.prefix.*` and
      `plugins.var.lua.tagattr.suffix.*` to add prefix and suffix to
      messages with certain tag.
    
    - rename option `plugins.var.lua.tagattr.tag.*` to
      `plugins.var.lua.tagattr.color.*`

--- tagattr/tagattr.lua
+++ tagattr/tagattr.lua
@@ -1,4 +1,4 @@
-attributes = {}
+colors, prefixes, suffixes = {}, {}, {}
 strip_colors = false
 
 function setup()
@@ -17,59 +17,102 @@ function get_last_segment(text)
    return text:match("([^%.]+)$")
 end
 
+function parse_option(name, value)
+   local kind, key = name:match("([^%.]+)%.([^%.]+)$")
+   if kind and key then
+      if kind == "misc" then
+         if kind == "strip_colors" then
+            strip_colors = (value == "1")
+         end
+      elseif kind == "color" then
+         colors[key] = value ~= "" and value or nil
+      elseif kind == "prefix" or kind == "suffix" then
+         if value == "" then
+            value = nil
+         else
+            local text, color = value:match("^([^;]+);(.+)$")
+            if not text or not color then
+               text, color = value, "default"
+            end
+            value = { text, color }
+         end
+
+         if kind == "prefix" then
+            prefixes[key] = value
+         else
+            suffixes[key] = value
+         end
+      end
+   end
+end
+
 function load_config()
-   local opt_filter = "plugins.var.lua.tagattr.tag.*"
+   local strip_colors_not_set = true
+   local opt_filter = "plugins.var.lua.tagattr.*"
    local opt_list = weechat.infolist_get("option", "", opt_filter)
+
    while weechat.infolist_next(opt_list) == 1 do
       local opt_name = weechat.infolist_string(opt_list, "option_name")
-      local tag = get_last_segment(opt_name)
-      attributes[tag] = weechat.infolist_string(opt_list, "value")
+      local opt_value = weechat.infolist_string(opt_list, "value")
+      parse_option(opt_name, opt_value)
    end
-   weechat.infolist_free(opt_list)
 
+   weechat.infolist_free(opt_list)
    if weechat.config_is_set_plugin("strip_colors") ~= 1 then
       weechat.config_set_plugin("strip_colors", 0)
-   else
-      local value = weechat.config_get_plugin("strip_colors")
-      strip_colors = value == "1"
    end
 end
 
 function config_cb(_, opt_name, opt_value)
-   if opt_name == "plugins.var.lua.tagattr.strip_colors" then
-      strip_colors = opt_value == "1"
-   elseif opt_name:find("plugins.var.lua.tagattr.tag.") then
-      local tag = get_last_segment(opt_name)
-      attributes[tag] = opt_value ~= "" and opt_value or nil
-   end
+   parse_option(opt_name, opt_value)
 end
 
 function print_cb(_, modifier, modifier_data, text)
    local _, _, tags = modifier_data:match("([^;]+);([^;]+);(.+)")
-   local attr = ""
-   local prefix, message = text:match("^([^\t]*\t)(.+)$")
-   if not message then
-      prefix, message = "", text
+   local left, right = text:match("^([^\t]*\t)(.+)$")
+   if not right then
+      left, right = "", text
    end
+
+   local msg_prefixes, msg_color, msg_suffixes = "", "", ""
+   local is_action = false
 
    if tags and tags ~= "" then
       for tag in tags:gmatch("([^,]+)") do
-         if attributes[tag] then
-            attr = attr .. weechat.color(attributes[tag])
-            if tag == "irc_action" then
-               local nick, actual_message = message:match("^([^%s]+%s+)(.+)$")
-               prefix = prefix .. nick
-               message = actual_message
-            end
+
+         if tag == "irc_action" then
+            is_action = true
          end
+
+         if colors[tag] then
+            msg_color = weechat.color(colors[tag])
+         end
+
+         if prefixes[tag] then
+            msg_prefixes = msg_prefixes ..
+                           weechat.color(prefixes[tag][2]) ..
+                           prefixes[tag][1] .. " "
+         end
+
+         if suffixes[tag] then
+            msg_suffixes = msg_suffixes .. " " ..
+                           weechat.color(suffixes[tag][2]) ..
+                           suffixes[tag][1]
+         end
+
       end
    end
 
    if strip_colors then
-      message = weechat.string_remove_color(message, "")
+      right = weechat.string_remove_color(right, "")
    end
-   local result = prefix .. attr .. message
-   return result
+   if is_action then
+      local nick, actual_message = right:match("^([^%s]+%s+)(.+)$")
+      return left .. msg_prefixes .. nick .. msg_color ..
+             actual_message .. msg_suffixes
+   else
+      return left .. msg_prefixes .. msg_color .. right .. msg_suffixes
+   end
 end
 
 setup()
