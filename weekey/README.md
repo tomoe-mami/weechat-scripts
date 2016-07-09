b18f2bc7c934eb25040f53ac0f056635ebe8e926
tomoe-mami <rumia.youkai.of.dusk@gmail.com>
2016-07-09 12:26:44 +0700

    weekey: hdata, overrides, and readme
    - replace all infolists with hdata
    - allow overriding key names using option
      `plugins.var.lua.weekey.name.*`
    - more text in readme

--- weekey/README.md
+++ weekey/README.md
@@ -3,7 +3,7 @@ weekey
 
 *(This script requires WeeChat 1.0 or newer)*
 
-Bar items for showing recently pressed key combo.
+Display recently pressed key combo and the command bound to it in bar items.
 
 The name of the bar items are:
 
@@ -11,3 +11,77 @@ The name of the bar items are:
 - **weekey_command**: The command for the key combo
 - **weekey_context**: Context of the key combo
 
+### Overriding combo name
+
+You can customize the combo name displayed on bar item **weekey_combo** with:
+
+    /set plugins.var.lua.weekey.name.<internal-weechat-key-code> <new-name>
+
+This is useful for when your terminal has key codes that this script doesn't
+recognize.
+
+Please note that internal Weechat key code is not the same as the code you get
+from `Alt-k` (or `/input grab_key_command`). Internal code starts with `\x01`.
+If it's followed by a single `[`, then it's the same as `meta-` key from `Alt-k`
+output. If there's another `[` after that, then it's a `meta2-`. If no `[` after
+`\x01` then it's a `ctrl-`. Some examples:
+
+Tab (`ctrl-I`):
+
+    /set plugins.var.lua.weekey.name.\x01I Tabulation
+
+Arrow up (`meta2-A`):
+
+    /set plugins.var.lua.weekey.name.\x01[[A ↑
+
+Alt-PageUp in XTerm (`meta2-5;3~`):
+
+    /set plugins.var.lua.weekey.name.\x01[[5;3~ You pressed Alt and PageUp!
+
+F1 in Tmux (`meta-OP`):
+
+    /set plugins.var.lua.weekey.name.\x01[OP Vroom! Vroom!
+
+
+### Options
+
+##### plugins.var.lua.weekey.duration
+
+Numbers of second the bar items should be displayed after user pressed a key
+combo.
+
+##### plugins.var.lua.weekey.mod_separator
+
+Separator between modifiers (`Ctrl`, `Meta`, `Shift`).
+
+##### plugins.var.lua.weekey.key_separator
+
+Character for separating multiple key codes in a combo (for example the default
+bind `Alt-j Alt-r`).
+
+##### plugins.var.lua.weekey.local_bindings
+
+Local key bindings are custom key bindings set on specific buffer. For example
+the `/script` buffer has Arrow Up key set for moving the highlight up. If this
+option is enabled, these local bindings will be shown on `weekey_*` bar items.
+
+##### plugins.var.lua.weekey.color_command
+
+Color for item `weekey_command`.
+
+
+##### plugins.var.lua.weekey.color_context
+
+Color for item `weekey_context`.
+
+##### plugins.var.lua.weekey.color_key
+
+Color for keys in item `weekey_combo`.
+
+##### plugins.var.lua.weekey.color_separator
+
+Color for key/modifier separator.
+
+##### plugins.var.lua.weekey.color_local_command
+
+Color for command from local bindings.
--- weekey/weekey.lua
+++ weekey/weekey.lua
@@ -1,13 +1,3 @@
--- A dumb script for displaying key combo in a bar
---
--- It creates 3 bar items:
---
--- 1. weekey_combo: the key combo
--- 2. weekey_command: the command for that key combo 
--- 3. weekey_context: context for the key combo
---
--- Requires Weechat 1.0 or newer
-
 w, script_name = weechat, "weekey"
 
 g = {
@@ -15,13 +5,11 @@ g = {
    recent_command = "",
    recent_context = "",
    hook = {},
-   modifier = {
-      shift = "Shift",
-      ctrl = "Ctrl",
-      meta = "Meta",
-      meta2 = "Meta2"
+   context = {
+      default = "gui_keys",
+      search = "gui_keys_search",
+      cursor = "gui_keys_cursor"
    },
-   context = { "default", "search", "cursor" },
    config = {},
    binding = {},
    default = {
@@ -33,40 +21,78 @@ g = {
       color_command = { "default", "Color of command" },
       color_local_command = { "cyan", "Color for local command" },
       color_key = { "yellow", "Color of key" },
-      color_delim = { "bar_delim", "Color of delimiter" }
+      color_separator = { "bar_delim", "Color of separator" }
    },
-   item = { "combo", "command", "context" }
+   item = { "combo", "command", "context" },
+   names = {
+      shift = "Shift",     ctrl = "Ctrl",
+      meta = "Meta",       meta2 = "Meta2",
+      up = "Up",           left = "Left",
+      down = "Down",       right = "Right",
+      tab = "Tab",         space = "Space",
+      backspace = "Back",  enter = "Enter",
+      insert = "Ins",      delete = "Del",
+      home = "Home",       ["end"] = "End",
+      pageup = "PgUp",     pagedown = "PgDown",
+      f1 = "F1",           f2 = "F2",
+      f3 = "F3",           f4 = "F4",
+      f5 = "F5",           f6 = "F6",
+      f7 = "F7",           f8 = "F8",
+      f9 = "F9",           f10 = "F10",
+      f11 = "F11",         f12 = "F12",
+   }
 }
 
 function main()
-   if w.register(script_name, "singalaut", "0.1", "WTFPL", "Key monitoring", "", "") then
+   local reg_ok = w.register(
+      script_name, "singalaut <https://github.com/tomoe-mami>", "0.1", "WTFPL",
+      "Display recently pressed key combo in bar items", "", "")
+
+   if reg_ok then
+      local wee_ver = tonumber(w.info_get("version_number", "") or 0)
+      if wee_ver < 0x01000000 then
+         w.print("", w.prefix("error")..script_name..": This script requires WeeChat v1.0 or newer.")
+         w.command("", "/wait 1ms /lua unload "..script_name)
+         return
+      end
+
       init_config()
       collect_bindings()
-
+      setup_key_map()
+      setup_key_map = nil
       for _, item in ipairs(g.item) do
          w.bar_item_new(script_name.."_"..item, "item_"..item.."_cb", "")
       end
-      for _, ctx in ipairs(g.context) do
+      for ctx, _ in pairs(g.context) do
          w.hook_signal("9000|key_combo_"..ctx, "key_combo_cb", ctx)
       end
+      -- w.hook_focus("chat", "focus_cb", "")
       w.hook_signal("key_bind", "key_bind_cb", "")
       w.hook_signal("key_unbind", "key_bind_cb", "")
       w.hook_config("plugins.var.lua."..script_name..".*", "config_cb", "")
+
+      w.hook_command(
+         script_name,
+         string.format([[
+The command /%s only dumps the list of known key combos (for debugging).
+
+If you need help on how to configure this script please see the README at
+https://github.com/tomoe-mami/weechat-scripts/tree/master/%s]], script_name, script_name),
+         "", "", "", "command_cb", "")
    end
 end
 
 function collect_bindings()
    local binds = g.binding
-   for _, ctx in ipairs(g.context) do
-      local list = w.infolist_get("key", "", ctx)
-      if list and list ~= "" then
-         binds[ctx] = {}
-         while w.infolist_next(list) == 1 do
-            local key_code = w.infolist_string(list, "key_internal")
-            local command = w.infolist_string(list, "command")
-            binds[ctx][key_code] = command
-         end
-         w.infolist_free(list)
+   local h_key = w.hdata_get("key")
+   for ctx, list_name in pairs(g.context) do
+      binds[ctx] = {}
+      local key = w.hdata_get_list(h_key, list_name)
+      while key and key ~= "" do
+         local key_code = w.hdata_string(h_key, key, "key")
+         local command = w.hdata_string(h_key, key, "command")
+         binds[ctx][key_code] = command
+         key = w.hdata_pointer(h_key, key, "next_key")
       end
    end
 end
@@ -122,17 +148,19 @@ function check_local_bindings()
 end
 
 function collect_local_bindings(buf_ptr)
-   local list = w.infolist_get("buffer", buf_ptr, "")
-   if list and list ~= "" and w.infolist_next(list) == 1 then
-      local fields = ","..w.infolist_fields(list)..","
-      local keys = {}
-      for id in fields:gmatch(",s:key_(%d+),") do
-         local code = w.infolist_string(list, "key_"..id)
-         keys[code] = w.infolist_string(list, "key_command_"..id)
-      end
-      w.infolist_free(list)
-      g.binding.buffer[buf_ptr] = keys
+   local h_buffer, h_key = w.hdata_get("buffer")
+   if w.hdata_integer(h_buffer, buf_ptr, "keys_count") < 1 then
+      g.binding.buffer[buf_ptr] = nil
+      return
    end
+   local h_key, keys = w.hdata_get("key"), {}
+   local key = w.hdata_pointer(h_buffer, buf_ptr, "keys")
+   while key ~= "" do
+      local code = w.hdata_string(h_key, key, "key")
+      keys[code] = w.hdata_string(h_key, key, "command")
+      key = w.hdata_pointer(h_key, key, "next_key")
+   end
+   g.binding.buffer[buf_ptr] = keys
 end
 
 function item_combo_cb()
@@ -160,7 +188,7 @@ function key_combo_cb(ctx, _, signal_data)
       g.hook.item_timer = nil
    end
    local found_bind, found_bind_ctx = get_binding(ctx, signal_data, buf_ptr)
-   update_recent_combo(signal_data, found_bind)
+   update_recent_combo(signal_data, found_bind, found_bind_ctx)
    update_recent_command(found_bind, found_bind_ctx)
    if g.config.duration > 0 then
       g.hook.item_timer = w.hook_timer(g.config.duration, 0, 1, "item_timer_cb", "")
@@ -207,7 +235,7 @@ function update_recent_command(cmd, ctx)
    end
 end
 
-function update_recent_combo(signal_data, found_bind)
+function update_recent_combo(signal_data, found_bind, ctx)
    local conf = g.config
    local color_key = w.color(conf.color_key)
 
@@ -220,9 +248,9 @@ function update_recent_combo(signal_data, found_bind)
       return
    end
 
-   local mod, keys = g.modifier, g.keys
+   local names, keys, overrides = g.names, g.keys, g.overrides
    local combo, i, partial = {}, 1, false
-   local color_delim = w.color(conf.color_delim)
+   local color_separator = w.color(conf.color_separator)
 
    for ch in signal_data:gmatch("(\001[^\001]*)") do
       local seq = {}
@@ -230,7 +258,10 @@ function update_recent_combo(signal_data, found_bind)
          i = i - 1
          table.insert(seq, combo[i])
       end
-      if keys[ch] then
+      if overrides[ch] then
+         table.insert(seq, overrides[ch])
+         partial = false
+      elseif keys[ch] then
          if type(keys[ch]) == "table" then
             for _, k in ipairs(keys[ch]) do
                table.insert(seq, k)
@@ -242,11 +273,11 @@ function update_recent_combo(signal_data, found_bind)
       else
          local brackets, str = ch:match("^\001(%[?%[?)(.*)$")
          if brackets == "[" then
-            table.insert(seq, mod.meta)
+            table.insert(seq, names.meta)
          elseif brackets == "[[" then
-            table.insert(seq, mod.meta2)
+            table.insert(seq, names.meta2)
          elseif brackets == "" then
-            table.insert(seq, mod.ctrl)
+            table.insert(seq, names.ctrl)
          end
          if str ~= "" then
             table.insert(seq, str)
@@ -255,13 +286,13 @@ function update_recent_combo(signal_data, found_bind)
             partial = true
          end
       end
-      combo[i] = color_key..table.concat(seq, color_delim..conf.mod_separator..color_key)
+      combo[i] = color_key..table.concat(seq, color_separator..conf.mod_separator..color_key)
       i = i + 1
    end
    if #combo == 0 then
       g.recent_combo = ""
    else
-      g.recent_combo = table.concat(combo, color_delim..conf.key_separator)
+      g.recent_combo = table.concat(combo, color_separator..conf.key_separator)
    end
 end
 
@@ -272,6 +303,25 @@ function item_timer_cb()
    g.recent_context = ""
    update_items()
    return w.WEECHAT_RC_OK
+end
+
+function focus_cb(_, info)
+   local binds, found_key, found_cmd, test_key = g.binding.cursor
+   if info._buffer_full_name then
+      test_key = string.format("@chat(%s):%s", cursor_key)
+      if binds[test_key] then
+         found_cmd = binds[test_key]
+         found_key = test_key
+      end
+   end
+   if not found_key then
+      test_key = string.format("@chat:%s", cursor_key)
+      if binds[test_key] then
+         found_cmd = binds[test_key]
+         found_key = test_key
+      end
+   end
+   return info
 end
 
 function key_bind_cb()
@@ -292,9 +342,13 @@ function key_bind_timer_cb(buf_ptr)
 end
 
 function config_cb(_, full_name, value)
-   local prefix = "plugins.var.lua."..script_name.."."
-   local name = full_name:sub(#prefix + 1)
-   if name and name ~= "" and g.default[name] then
+   local name = full_name:gsub("^plugins%.var%.lua%."..script_name.."%.", "")
+   if name:sub(1, 5) == "name." then
+      local key_code = convert_escape(name:sub(6))
+      if key_code ~= "" then
+         g.overrides[key_code] = value ~= "" and value or nil
+      end
+   elseif g.default[name] then
       g.config[name] = value
       if name == "duration" then
          validate_duration()
@@ -319,60 +373,154 @@ function buffer_switch_cb(_, _, buf_ptr)
    return w.WEECHAT_RC_OK
 end
 
--- these are just partial key list.
--- if you want to add another key the generic guides are:
---
--- weechat modifier           internal key code
--- ============================================
--- meta2-                     \001[[
--- meta-                      \001[
--- ctrl-                      \001
--- 
--- of course there are exceptions because terminal keys are fucking annoying
+function command_cb(_, buffer, param)
+   local names, keys, overrides, conf = g.names, g.keys, g.overrides, g.config
+   local delim, reset = w.color("chat_delimiters"), w.color("reset")
+   local o = {}
+   for k, v in pairs(keys) do
+      local code = k:gsub("\001%[", delim.."^["..reset)
+                    :gsub("\001", delim.."^"..reset)
 
-g.keys = {
-   ["\001?"] = "Back",
-   ["\001H"] = { g.modifier.ctrl, "Back" },
-   ["\001I"] = "Tab",
-   ["\001M"] = "Enter",
-   ["\001@"] = "Space",
-   ["\001[[A"] = "Up",
-   ["\001[[B"] = "Down",
-   ["\001[[C"] = "Right",
-   ["\001[[D"] = "Left",
-   ["\001[[Z"] = { g.modifier.shift, "Tab" },
-   ["\001[OA"] = { g.modifier.ctrl, "Up" },
-   ["\001[OB"] = { g.modifier.ctrl, "Down" },
-   ["\001[OC"] = { g.modifier.ctrl, "Right" },
-   ["\001[OD"] = { g.modifier.ctrl, "Left" },
-   ["\001[ "] = { g.modifier.meta, "Space" },
+      local str
+      if type(v) == "table" then
+         str = table.concat(v, conf.mod_separator)
+      else
+         str = v
+      end
+      table.insert(o, code.."\t"..str)
+   end
+   table.sort(o)
+   for _, v in ipairs(o) do
+      w.print("", v)
+   end
+   return w.WEECHAT_RC_OK
+end
 
-   ["\001[OP"] = "F1",
-   ["\001[OQ"] = "F2",
-   ["\001[OR"] = "F3",
-   ["\001[OS"] = "F4",
-   ["\001[[15~"] = "F5",
-   ["\001[[17~"] = "F6",
-   ["\001[[18~"] = "F7",
-   ["\001[[19~"] = "F8",
-   ["\001[[20~"] = "F9",
-   ["\001[[21~"] = "F10",
-   ["\001[[23~"] = "F11",
-   ["\001[[24~"] = "F12",
+function setup_key_map()
+   local n, term = g.names, os.getenv("TERM")
+   local keys = {
+      ["\001I"] = n.tab,
+      ["\001M"] = n.enter,
+      ["\001@"] = { n.ctrl, n.space},
+      ["\001[[A"] = n.up,
+      ["\001[[B"] = n.down,
+      ["\001[[C"] = n.right,
+      ["\001[[D"] = n.left,
+      ["\001[[Z"] = { n.shift, n.tab },
+      ["\001[OA"] = { n.ctrl, n.up },
+      ["\001[OB"] = { n.ctrl, n.down },
+      ["\001[OC"] = { n.ctrl, n.right },
+      ["\001[OD"] = { n.ctrl, n.left },
+      ["\001[ "] = { n.meta, n.space },
 
-   ["\001[[1~"] = "Home",
-   ["\001[[2~"] = "Ins",
-   ["\001[[3~"] = "Del",
-   ["\001[[4~"] = "End",
-   ["\001[[5~"] = "PgDown",
-   ["\001[[6~"] = "PgUp",
+      ["\001[OP"] = n.f1,
+      ["\001[OQ"] = n.f2,
+      ["\001[OR"] = n.f3,
+      ["\001[OS"] = n.f4,
+      ["\001[[15~"] = n.f5,
+      ["\001[[17~"] = n.f6,
+      ["\001[[18~"] = n.f7,
+      ["\001[[19~"] = n.f8,
+      ["\001[[20~"] = n.f9,
+      ["\001[[21~"] = n.f10,
+      ["\001[[23~"] = n.f11,
+      ["\001[[24~"] = n.f12,
 
-   -- xterm
+      ["\001[[1~"] = n.home,
+      ["\001[[2~"] = n.insert,
+      ["\001[[3~"] = n.delete,
+      ["\001[[4~"] = n["end"],
+      ["\001[[5~"] = n.pageup,
+      ["\001[[6~"] = n.pagedown,
 
-   ["\001[[H"] = "Home",
-   ["\001[[F"] = "End",
-   ["\001[[2;5~"] = { g.modifier.ctrl, "Ins" },
-   ["\001[[3;5~"] = { g.modifier.ctrl, "Del" },
-}
+      ["\001[[H"] = n.home,
+      ["\001[[F"] = n["end"]
+   }
+
+   local xterm_mods = {
+      [2] = { n.shift },
+      [3] = { n.meta },
+      [4] = { n.meta, n.shift },
+      [5] = { n.ctrl },
+      [6] = { n.ctrl, n.shift },
+      [7] = { n.meta, n.ctrl },
+      [8] = { n.meta, n.ctrl, n.shift },
+   }
+
+   local add = function (t1, t2, ...)
+      local t = {}
+      for _, v in ipairs(t1) do
+         table.insert(t, v)
+      end
+      for _, v in ipairs(t2) do
+         table.insert(t, v)
+      end
+      keys[string.format(...)] = t
+   end
+
+   for i, m in pairs(xterm_mods) do
+      add(m, { n.up }, "\001[[1;%sA", i)
+      add(m, { n.down }, "\001[[1;%sB", i)
+      add(m, { n.right }, "\001[[1;%sC", i)
+      add(m, { n.left }, "\001[[1;%sD", i)
+
+      add(m, { n.home }, "\001[[1;%sH", i)
+      add(m, { n["end"] }, "\001[[1;%sF", i)
+
+      add(m, { n.insert }, "\001[[2;%s~", i)
+      add(m, { n.delete }, "\001[[3;%s~", i)
+      add(m, { n.pageup }, "\001[[5;%s~", i)
+      add(m, { n.pagedown }, "\001[[6;%s~", i)
+   end
+
+   -- FIXME: this sucks
+   if term:match("^xterm") then
+      keys["\001H"] = n.backspace
+      keys["\001?"] = { n.ctrl, n.backspace }
+   else
+      keys["\001?"] = n.backspace
+      keys["\001H"] = { n.ctrl, n.backspace }
+   end
+   g.keys = keys
+   g.overrides = collect_overrides()
+end
+
+function convert_escape(s)
+   s = s:gsub("(\\+)([xX]%x%x?)", function (b, n)
+              if #b % 2 == 0 then
+                 return b..n
+              else
+                 return b:sub(2)..string.char(tonumber(n:sub(2), 16))
+              end
+           end)
+   return (s:gsub("\\\\", "\\"))
+end
+
+function collect_overrides()
+   local h_file = w.hdata_get("config_file")
+   local file = w.hdata_search(h_file,
+                               w.hdata_get_list(h_file, "config_files"),
+                               "${config_file.name} == plugins", 1)
+   if not file or file == "" then
+      return
+   end
+   local section = w.config_search_section(file, "var")
+   if not section or section == "" then
+      return
+   end
+   local h_option, keys = w.hdata_get("config_option"), {}
+   local option = w.hdata_pointer(w.hdata_get("config_section"), section, "options")
+   local opt_prefix = "lua."..script_name..".name."
+   while option and option ~= "" do
+      local name = w.hdata_string(h_option, option, "name")
+      if #name > #opt_prefix and name:sub(1, #opt_prefix) == opt_prefix then
+         local key_code = convert_escape(name:sub(#opt_prefix + 1))
+         local seq = w.config_string(option)
+         keys[key_code] = seq
+      end
+      option = w.hdata_pointer(h_option, option, "next_option")
+   end
+   return keys
+end
 
 main()
