7b182b78973fbb8d19e9e7f55b7774a5a0c364be
tomoe-mami <rumia.youkai.of.dusk@gmail.com>
2017-10-25 16:47:18 +0700

    empty_complete: cleanup
    - replace dumb loop with iterator
    - merge find_server into get_recent_speaker
    - rename some variables

--- empty_complete/empty_complete.lua
+++ empty_complete/empty_complete.lua
@@ -50,52 +50,44 @@ function config_cb(_, opt_name, opt_value)
    return w.WEECHAT_RC_OK
 end
 
-function find_server(server_name)
+function get_recent_speakers(server_name, channel_name)
    local h_server = w.hdata_get("irc_server")
    local ptr_server = w.hdata_get_list(h_server, "irc_servers")
-   while ptr_server and ptr_server ~= "" do
+   while ptr_server ~= "" do
       if w.hdata_string(h_server, ptr_server, "name") == server_name then
-         return h_server, ptr_server
+         local h_channel = w.hdata_get("irc_channel")
+         local ptr_channel = w.hdata_pointer(h_server, ptr_server, "channels")
+         while ptr_channel ~= "" do
+            if w.hdata_string(h_channel, ptr_channel, "name") == channel_name then
+               return w.hdata_pointer(h_channel, ptr_channel, "nicks_speaking")
+            end
+            ptr_channel = w.hdata_pointer(h_channel, channel, "next_channel")
+         end
+         break
       end
       ptr_server = w.hdata_pointer(h_server, ptr_server, "next_server")
    end
 end
 
-function get_recent_speakers(server_name, channel_name)
-   local h_server, server = find_server(server_name)
-   if not server or server == "" then
-      return
-   end
-
-   local h_channel = w.hdata_get("irc_channel")
-   local channel, found = w.hdata_pointer(h_server, server, "channels"), false
-   while channel and channel ~= "" do
-      if w.hdata_string(h_channel, channel, "name") == channel_name then
-         return w.hdata_pointer(h_channel, channel, "nicks_speaking")
-      end
-      channel = w.hdata_pointer(h_channel, channel, "next_channel")
-   end
-end
-
-function complete_cb(_, ptr_buf, cmd)
-   if w.buffer_get_string(ptr_buf, "plugin") ~= "irc" or
-      w.buffer_get_string(ptr_buf, "localvar_type") ~= "channel" then
+function complete_cb(_, ptr_buffer, cmd)
+   if w.buffer_get_string(ptr_buffer, "plugin") ~= "irc" or
+      w.buffer_get_string(ptr_buffer, "localvar_type") ~= "channel" then
       return w.WEECHAT_RC_OK
    end
    local dir = cmd:match("^/input complete_(.+)$")
    if dir ~= "next" and dir ~= "previous" then
       return w.WEECHAT_RC_OK
    end
-   local input_text = w.buffer_get_string(ptr_buf, "input")
+   local input_text = w.buffer_get_string(ptr_buffer, "input")
    if w.string_is_command_char(input_text) == 1 then
       return w.WEECHAT_RC_OK
    end
 
-   local input_length = w.buffer_get_integer(ptr_buf, "input_size")
-   local server_name = w.buffer_get_string(ptr_buf, "localvar_server")
-   local channel_name = w.buffer_get_string(ptr_buf, "localvar_channel")
+   local input_length = w.buffer_get_integer(ptr_buffer, "input_size")
+   local server_name = w.buffer_get_string(ptr_buffer, "localvar_server")
+   local channel_name = w.buffer_get_string(ptr_buffer, "localvar_channel")
 
-   local buffer = g.buffers[ptr_buf]
+   local buffer = g.buffers[ptr_buffer]
    if not buffer then
       if input_length > 0 then
          return w.WEECHAT_RC_OK
@@ -104,77 +96,80 @@ function complete_cb(_, ptr_buf, cmd)
       if not buffer.speakers or buffer.speakers == "" then
          return w.WEECHAT_RC_OK
       end
-      g.buffers[ptr_buf] = buffer
+      g.buffers[ptr_buffer] = buffer
    elseif input_length == 0 then
       buffer.index = nil
    elseif input_text ~= buffer.last_nick then
-      g.buffers[ptr_buf] = nil
+      g.buffers[ptr_buffer] = nil
       return w.WEECHAT_RC_OK
    end
 
-   local nick = get_speaker(ptr_buf, buffer, dir)
+   local nick = get_speaker(ptr_buffer, buffer, dir)
    if nick and nick ~= "" then
       buffer.last_nick = nick..w.config_string(g.config.nick_completer)
       if g.config.ncw_compat then
-         buffer.last_nick = w.buffer_get_string(ptr_buf, "localvar_ncw_prefix")..
+         buffer.last_nick = w.buffer_get_string(ptr_buffer, "localvar_ncw_prefix")..
                             buffer.last_nick..
-                            w.buffer_get_string(ptr_buf, "localvar_ncw_suffix")
+                            w.buffer_get_string(ptr_buffer, "localvar_ncw_suffix")
       end
       if w.config_boolean(g.config.nick_space) == 1 then
          buffer.last_nick = buffer.last_nick.." "
       end
-      w.buffer_set(ptr_buf, "input", buffer.last_nick)
-      w.command(ptr_buf, "/input move_end_of_line")
+      w.buffer_set(ptr_buffer, "input", buffer.last_nick)
+      w.command(ptr_buffer, "/input move_end_of_line")
    end
    return w.WEECHAT_RC_OK
 end
 
-function get_speaker(ptr_buf, buffer, dir)
-   local total = w.list_size(buffer.speakers)
+function list_items(ptr_list, start_pos, dir)
+   local total = w.list_size(ptr_list)
    if total == 0 then
       return
    end
-
-   local last = total - 1
-   if not buffer.index then
-      buffer.index = dir == "next" and last or 0
-   else
-      buffer.index = (buffer.index + (dir == "next" and -1 or 1)) % total
-   end
-
-   if not g.config.ignore_offline_nicks then
-      return w.list_string(w.list_get(buffer.speakers, buffer.index))
-   else
-      local p
-      if dir == "next" then
-         p = {{ buffer.index, 0, -1 }, { 0, buffer.index - 1, 1 }}
-      else
-         p = {{ buffer.index, last , 1 }, { last, buffer.index + 1, -1 }}
+   local c = 0
+   local step = dir == "next" and 1 or -1
+   return function ()
+      if c >= total then
+         return
       end
+      local pos = (start_pos + (c * step)) % total
+      local ptr_item = w.list_get(ptr_list, pos)
+      c = c + 1
+      if ptr_item == "" then
+         return
+      else
+         return pos, ptr_item
+      end
+   end
+end
 
-      for _, a in ipairs(p) do
-         for i = a[1], a[2], a[3] do
-            local val = w.list_string(w.list_get(buffer.speakers, i))
-            if w.nicklist_search_nick(ptr_buf, "", val) ~= "" then
-               buffer.index = i
-               return val
-            end
+function get_speaker(ptr_buffer, buffer, dir)
+   local nick
+   for pos, ptr_item in list_items(buffer.speakers, buffer.index, dir) do
+      if not g.config.ignore_offline_nicks then
+         buffer.index = pos
+         return w.list_string(ptr_item)
+      else
+         local nick = w.list_string(ptr_item)
+         if w.nicklist_search_nick(ptr_buffer, "", nick) ~= "" then
+            buffer.index = pos
+            return nick
          end
       end
    end
 end
 
-function history_add_cb(_, _, ptr_buf, text)
-   if g.buffers[ptr_buf] then
-      g.buffers[ptr_buf].index = nil
-      g.buffers[ptr_buf].last_nick = nil
+function history_add_cb(_, _, ptr_buffer, text)
+   if g.buffers[ptr_buffer] then
+      g.buffers[ptr_buffer].index = nil
+      g.buffers[ptr_buffer].last_nick = nil
    end
    return text
 end
 
-function buffer_closed_cb(_, _, ptr_buf)
-   if g.buffers[ptr_buf] then
-      g.buffers[ptr_buf] = nil
+function buffer_closed_cb(_, _, ptr_buffer)
+   if g.buffers[ptr_buffer] then
+      g.buffers[ptr_buffer] = nil
    end
    return w.WEECHAT_RC_OK
 end
